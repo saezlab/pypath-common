fc9989e935b11d723c44fcb92dd2ddd0cc5a59b1
deeenes <turei.denes@gmail.com>
2023-10-02 17:43:01 +0200

    uniform headers for py files

--- pypath_common/data/__init__.py
+++ pypath_common/data/__init__.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python
+
+#
+#  This file is part of the `pypath` python module
+#
+#  Copyright 2014-2023
+#  EMBL, EMBL-EBI, Uniklinik RWTH Aachen, Heidelberg University
+#
+#  Authors: see the file `README.rst`
+#  Contact: Dénes Türei (turei.denes@gmail.com)
+#
+#  Distributed under the GPLv3 License.
+#  See accompanying file LICENSE.txt or copy at
+#      https://www.gnu.org/licenses/gpl-3.0.html
+#
+#  Website: https://pypath.omnipathdb.org/
+#
+#
+#  This file is part of the `pypath` python module
+#
+
 """
 Data shared across pypath modules.
 
